d3f918cfa02f17248f08e809732aec6f6ff31cf3
NachoCorreas <icorreas.ig@gmail.com>
2023-09-14 23:50:02 -0300

    probando

---
+++ src/componentes/ListaNav/Lista.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./style.css"
+export default function () {
+  return (
+    <div className='lista'>
+        <ul>
+            <li>
+                <button>inicio</button>
+            </li>
+            <li>
+            <button>compra</button>
+            </li>
+            <li>
+            <button>detalle</button>
+            </li>
+        </ul>
+    </div>
+  )
+}
---
+++ src/componentes/Navbar/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+import Lista from '../ListaNav/Lista'
+export default function Nav() {
+  return (
+    <div className='navbar'>
+        <Lista/>
+    </div>
+  )
+}
