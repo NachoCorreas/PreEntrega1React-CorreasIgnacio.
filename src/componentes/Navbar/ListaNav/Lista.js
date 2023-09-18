7254bf8c6033bba3a05c88ca7969a62b7a444470
NachoCorreas <icorreas.ig@gmail.com>
2023-09-18 19:02:10 -0300

    boton mas img

--- src/componentes/Navbar/ListaNav/Lista.js
+++ src/componentes/Navbar/ListaNav/Lista.js
@@ -1,17 +1,41 @@
-import React from 'react'
+
 import "./style.css"
-export default function Lista() {
-  return (
-    <div className='lista'>
-      
-            <button>Zapatillas</button>
-            <button>Conjuntos</button>
-            <button>Accesorios</button>
-     
+import React, { Component } from 'react';
+
+class Lista extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ImagenActual: null,
+    };
+  }
+
+  ClickNav = (nomImg) => {
+    this.setState({ ImagenActual: nomImg });
+  }
+
+  render() {
+    const { ImagenActual } = this.state;
+
+    return (
+      <div className="lista">
+        <button  onClick={() => this.ClickNav('zapatillas')}>Zapatillas</button>
+        <button onClick={() => this.ClickNav('conjuntos')}>Conjuntos</button>
+        <button onClick={() => this.ClickNav('accesorios')}>Accesorios</button>
+
+        <div>
+          {ImagenActual && (
+            <img
+            src={require(`./img/${ImagenActual}.png`)}
            
-     
-    
- 
-    </div>
-  )
+              alt={ImagenActual}
+              style={{ maxWidth: '100px' }}
+            />
+          )}
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Lista;
